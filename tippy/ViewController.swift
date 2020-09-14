26545e8824d9ddc0c209c44f7351009b47c4343d
1nsekhon <63224811+1nsekhon@users.noreply.github.com>
2020-09-14 09:07:18 -0700

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Nanki Sekhon on 9/14/20.
+//  Copyright © 2020 Nanki Sekhon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
